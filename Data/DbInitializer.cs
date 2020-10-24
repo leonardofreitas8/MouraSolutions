162897c9cc13d4dac5e72f7ed57c517ddc9141ea
Leonardo <leonardofreitas8@gmail.com>
2020-10-24 05:41:32 -0300

    versionando codigo de deploy

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,30 @@
+﻿using MouraSolutionsWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MouraSolutionsWeb.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(MouraExpressContext context)
+        {
+            //context.Database.EnsureCreated();
+
+            if (context.Material.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var material = new Material[]
+            {
+            new Material { Descricao="Teste novo material"}
+            };
+            foreach (Material m in material)
+            {
+                context.Material.Add(m);
+            }
+        }
+    }
+}
---
+++ Data/MouraExpressContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MouraSolutionsWeb.Models;
+
+namespace MouraSolutionsWeb.Data
+{
+    public class MouraExpressContext : DbContext
+    {
+        //public MouraExpressContext()
+        //// C# will call base class parameterless constructor by default
+        //{
+            
+        //}
+        public MouraExpressContext(DbContextOptions<MouraExpressContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Material> Material { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Material>().ToTable("Material");
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+
+}
